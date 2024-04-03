5f70b2c6c7c8bf194fe480ef80a8c486a9fd07a6
Ali <87234097+MianajiAli@users.noreply.github.com>
2024-04-03 19:52:29 +0330

    github pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className='w-full bg-primary  text-s sm:text-xl items-center flex flex-col lg:flex-row lg:justify-between p-[4rem]'>
-            <p className='text-white font-Poppins5 '>Copyright © 2024. All rights are reserved</p>
+            <p className='text-white font-Poppins5 text-center'>Copyright © 2024. All rights are reserved</p>
             <div className='text-white font-Poppins5 flex pt-10 lg:pt-0'>
                 <p className='px-3'>Github</p>
                 <p className='px-3'>Linkdin</p>
