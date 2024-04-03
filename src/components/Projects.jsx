841f4ae9e076a9b83d771a6f0258e11efc77a890
Ali <87234097+MianajiAli@users.noreply.github.com>
2024-04-03 19:34:03 +0330

    fixing id

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
     return (
         <div>
-            <div id='about' className='max-w-[950px] w-full h-screen flex flex-col justify-center items-center mx-auto pt-10 lg:pt-0 '>
+            <div id='projects' className='max-w-[950px] w-full h-screen flex flex-col justify-center items-center mx-auto pt-10 lg:pt-0 '>
                 <div className='flex flex-row items-center text-center'>
 
                     <h3 className='text-2xl sm:text-4xl lg:text-6xl  text-primary font-Poppins7'>New projects coming soon.. 🤠</h3>
